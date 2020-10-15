adf59bc151a5b736e53fbc69556cf6293dc29321
Abe Tishelman-Charny <abe2323@yahoo.com>
2020-10-15 17:57:12 +0200

    Added Sig, Data, Bkg specifications to SkimFiles

--- NtupleAnalysis/SkimFiles.py
+++ NtupleAnalysis/SkimFiles.py
@@ -26,20 +26,27 @@ from python.MCTools import GetMCTreeName
 parser = argparse.ArgumentParser()
 parser.add_argument('--inDir', type=str, default="", help="Comma separated list of ntuple folders", required=False)
 parser.add_argument('--outDir', type=str, default="", help="Comma separated list of ntuple folders", required=False)
+parser.add_argument('--fileType', type=str, default="", help="File type: Data, Signal, or Bkg", required=True)
 parser.add_argument("--doppRemoval", action="store_true", default=False, help="Remove prompt-prompt events", required=False)
 parser.add_argument("--oneFile", action="store_true", default=False, help="Only run on one file (helpful for quicker testing)", required=False)
 # parser.add_argument("--mergeTags", action="store_true", default=False, help="Merge multiple trees into one tree", required=False) # not implemented yet. merges be default 
 args = parser.parse_args()
 
 ##-- Set variables to user args 
-argNames = ['inDir','outDir','doppRemoval','oneFile']
+argNames = ['inDir','outDir','doppRemoval','oneFile','fileType']
 for argName in argNames:
   exec("%s = args.%s"%(argName,argName))
   exec("print '%s :', %s"%(argName,argName))
 
+##-- Temporary: Define tags list 
+tags = []
+if(fileType == "Signal"): tags = [0,1,2,3,4]
+else: tags = [0,1,2]
+
 ##-- Define tree selection
 selection = ""
 if(doppRemoval): selection = "(!((Leading_Photon_genMatchType == 1) && (Subleading_Photon_genMatchType == 1)))" # selection: remove events where both photons are prompt
+print"Selection:",selection 
 
 ##-- Perform actions on files in input directory
 for inFileName in os.listdir(inDir): 
@@ -50,11 +57,12 @@ for inFileName in os.listdir(inDir):
 
   ##-- Open file, get name, instantiate TChain for merging 
   inFile = TFile.Open(inFilePath,"READ")
-  MCTreeName = GetMCTreeName(inFileName)
+  if(fileType!="Data"): MCTreeName = GetMCTreeName(inFileName)
+  else: MCTreeName = "Data"
   allTags_skimmedTrees = TChain(MCTreeName,MCTreeName)
 
   ##-- For each tree in input file 
-  for tag_i in [0,1,2]: ##-- "Tag" naming is specific to HHWWgg flashgg tagger 
+  for tag_i in tags: ##-- "Tag" naming is specific to HHWWgg flashgg tagger 
     print"On Tag ",tag_i
     tagOutFileName = "Tag_%s_skimmed_%s"%(tag_i,inFileName.split('/')[-1])
     tagOutFilePath = "%s/%s"%(outDir,tagOutFileName)
@@ -62,7 +70,10 @@ for inFileName in os.listdir(inDir):
     tagOutFile.mkdir("tagsDumper")
     tagOutFile.mkdir("tagsDumper/trees")
     tagOutFile.cd("tagsDumper/trees")
-    treeName = "tagsDumper/trees/%s_13TeV_HHWWggTag_%s"%(MCTreeName,str(tag_i))
+    if(fileType == "Data"): treeName = "tagsDumper/trees/Data_13TeV_HHWWggTag_%s"%(str(tag_i))
+    elif(fileType == "Signal"): treeName = "tagsDumper/trees/GluGluToHHTo_WWgg_qqlnu_nodeSM_13TeV_HHWWggTag_%s"%(str(tag_i))
+    else: treeName = "tagsDumper/trees/%s_13TeV_HHWWggTag_%s"%(MCTreeName,str(tag_i))
+    
     originalTree = inFile.Get(treeName)
     selectedTree = originalTree.CopyTree(selection)
     selectedTree.Write()
@@ -102,7 +113,7 @@ for inFileName in os.listdir(inDir):
   inFile.Close()
 
   ##-- Delete temporary tag files (one for each tree)
-  for tag_i in [0,1,2]: ##-- "Tag" naming is specific to HHWWgg flashgg tagger 
+  for tag_i in tags: ##-- "Tag" naming is specific to HHWWgg flashgg tagger 
     tagOutFileName = "Tag_%s_skimmed_%s"%(tag_i,inFileName.split('/')[-1])
     tagOutFilePath = "%s/%s"%(outDir,tagOutFileName)    
     os.system("rm %s"%(tagOutFilePath))
