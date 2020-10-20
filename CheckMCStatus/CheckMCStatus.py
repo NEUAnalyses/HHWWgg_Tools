c18ef2054f75ab5071611346b4b82e44a7cac1b2
Abe Tishelman-Charny <abe2323@yahoo.com>
2020-10-20 08:56:38 +0200

    Made CheckMC Code a bit more General

--- CheckMCStatus/CheckMCStatus.py
+++ CheckMCStatus/CheckMCStatus.py
@@ -1,41 +1,77 @@
+########################################################################################################################
+# Abraham Tishelman-Charny                                                                                             #
+# 20 October 2020                                                                                                      #
+#                                                                                                                      #
+# The purpose of this module is to plot MetaData for CMS DAS samples of interest. Can for example                      #
+# be used to check the MC status of centrally produced samples by plotting number of events.                           #
+#                                                                                                                      #
+# Example Usage:                                                                                                       #
+#                                                                                                                      #
+# voms # (voms-proxy-init --voms cms --valid 168:00)                                                                   #
+# python CheckMCStatus.py --outLoc /eos/user/a/atishelm/www/HHWWgg/MCstatus --oneDataset --PhysicsType NonResonant     #
+########################################################################################################################
+
 from python.CheckMCStatus_Tools import * 
+from python.Options import * 
 import pandas as pd 
+import subprocess
 
-ol = "/eos/user/a/atishelm/www/HHWWgg/MCstatus"
+##-- Get User input Args
+args = GetOptions() 
 
-if(not os.path.exists(ol)):
-    print"Creating output directory: %s"%(ol)
-    beforeOl = "%s/../"%(ol)
-    os.system('mkdir %s'%(ol))
-    os.system('cp %s/index.php %s'%(beforeOl,ol))   
+if __name__ == '__main__':
+    oneDataset = args.oneDataset
+    
+    ##-- Set output location and create directory if it doesn't exist, copying index file as well 
+    ##-- Note: <outLoc>/../ must exist 
+    outLoc = args.outLoc  
+    CheckMakeOutLocation(outLoc)
 
-# os.system("echo'Initializing voms proxy...'")
-# os.system("voms-proxy-init --voms cms --valid 168:00")
+    ##-- Define Dataset Names based on physics process, type, year, step 
+    pDsetNames, Processes, sDsetNames, steps, instances, outDsetNames = [], [], [], [], [], []
+    finalStates = ["GluGluToHHTo2G2Qlnu*", "GluGluToHHTo2G4Q*", "GluGluToHHTo2G2l2nu*"]
+    years = ["*RunIISummer16MiniAODv3*", "*RunIIFall17MiniAODv2*", "*RunIIAutumn18MiniAOD*"]
+    step = "MINIAODSIM"
+    instance = "prod/global"
+    Processes_ = GetProcesses("NonResonant")
 
-pDsetNames, sDsetNames, steps, instances, outDsetNames = [], [], [], [], []
-finalStates = ["GluGluToHHTo2G2Qlnu*", "GluGluToHHTo2G4Q*", "GluGluToHHTo2G2l2nu*"]
-years = ["*RunIISummer16MiniAODv3*", "*RunIIFall17MiniAODv2*", "*RunIIAutumn18MiniAOD*"]
-step = "MINIAODSIM"
-instance = "prod/global"
+    for Process in Processes_:
+        for finalState in finalStates:
+            for year in years:
+                fState_fsetStr = "%s%s"%(finalState,Process)
+                pDsetNames.append(fState_fsetStr)
+                sDsetNames.append(year)
+                steps.append(step)
+                instances.append(instance)
+                outName_Process = GetProcessLabel(Process)
+                Processes.append(outName_Process)
+                outName_FS = GetFSLabel(finalState)
+                outName_year = GetYearLabel(year)
+                outDsetName = "%s-%s-%s"%(outName_Process,outName_FS,outName_year)
+                outDsetNames.append(outDsetName)
+                if(oneDataset): break
+            if(oneDataset): break 
 
-for finalState in finalStates:
-    for year in years:
-        pDsetNames.append(finalState)
-        sDsetNames.append(year)
-        steps.append(step)
-        instances.append(instance)
-        outName_FS = GetFSLabel(finalState)
-        outName_year = GetYearLabel(year)
-        outDsetName = "%s-%s"%(outName_FS,outName_year)
-        outDsetNames.append(outDsetName)
+    ##-- Query DAS and plot MetaData for each dataset name 
+    for i in range(len(pDsetNames)):
+        paramNames = ["pDsetName", "Process", "sDsetName", "step", "instance", "outDsetName"]
+        for paramName in paramNames:
+            if(paramName.endswith('s')):
+                exec("%s = %ses[i]"%(paramName,paramName))
+            else: 
+                exec("%s = %ss[i]"%(paramName,paramName))
+        check = CheckMC(pDsetName, sDsetName, step, instance, outDsetName)
+        check.GetDatasetNames() ##-- Get dataset names 
+        command = "wc -l Datasets/%s.txt"%(outDsetName)
+        nDatasets = int(str(subprocess.check_output(command, shell=True)).split(' ')[0])
+        print "nDatasets:",nDatasets
+        if(nDatasets > 0): ##-- If the query returns no datasets, cannot plot anything 
+            dsets = pd.read_csv('Datasets/%s.txt'%(outDsetName), header=None)
+            check.GetDsetMetaData(dsets) ##-- Produce txt file with number of events per dataset 
 
-for i in range(len(pDsetNames)):
-    paramNames = ["pDsetName", "sDsetName", "step", "instance", "outDsetName"]
-    for paramName in paramNames:
-        exec("%s = %ss[i]"%(paramName,paramName))
-
-    check = CheckMC(pDsetName, sDsetName, step, instance, outDsetName)
-    check.GetDatasetNames() ##-- Get dataset names 
-    dsets = pd.read_csv('%s.txt'%(outDsetName), header=None)
-    check.GetDsetMetaData(dsets) ##-- Produce txt file with number of events per dataset 
-    check.Plot(dsets,ol)
+            ##-- Plot 
+            if(Process=="GF"):
+                check.Plot(dsets,outLoc,"GF_LO")
+                check.Plot(dsets,outLoc,"GF_NLO")
+            if(Process=="VBF"):
+                check.Plot(dsets,outLoc,"VBF_LO")
--- CheckMCStatus/python/CheckMCStatus_Tools.py
+++ CheckMCStatus/python/CheckMCStatus_Tools.py
@@ -1,3 +1,11 @@
+########################################################################################################################
+# Abraham Tishelman-Charny                                                                                             #
+# 20 October 2020                                                                                                      #
+#                                                                                                                      #
+# The purpose of this module is to provide a class and methods for a CheckMC object to be used by the                  #
+# CheckMCStatus module.                                                                                                #                                                                                                          
+########################################################################################################################
+
 import os 
 import pandas as pd 
 from matplotlib import pyplot as plt 
@@ -14,31 +22,35 @@ class CheckMC:
 
     def GetDatasetNames(self):
         print"Querying DAS..."
-        os.system("rm %s.txt"%(self.outDsets))
-        dasCommand = "dasgoclient --query='/%s/%s/%s instance=%s' >> %s.txt"%(self.primaryDatasetName, self.secondaryDatasetName, self.step, self.instance, self.outDsets)
+        if(not os.path.exists("Datasets")):
+            print"Creating output directory: Datasets"
+            os.system('mkdir Datasets')   
+        os.system("rm Datasets/%s.txt"%(self.outDsets))
+        dasCommand = "dasgoclient --query='/%s/%s/%s instance=%s' >> Datasets/%s.txt"%(self.primaryDatasetName, self.secondaryDatasetName, self.step, self.instance, self.outDsets)
         print"DAS Command:",dasCommand
         os.system(dasCommand)
         print"DAS Command:",dasCommand
 
     def GetDsetMetaData(self, dsets):
         grepString = "nevents"
-        outFile = "%s_MetaData.txt"%(self.outDsets)
+        if(not os.path.exists("MetaData")):
+            print"Creating output directory: MetaData"
+            os.system('mkdir MetaData')
+        outFile = "MetaData/%s_MetaData.txt"%(self.outDsets)
         outLabel = "MetaData"
         os.system("rm %s"%(outFile))
         for dset in dsets[0]:
             print"dset:",dset
             dasCommand = "dasgoclient --query 'summary dataset=%s instance=%s' -json | grep %s >> %s"%(dset,self.instance, grepString, outFile)
-            os.system(dasCommand)
+            os.system(dasCommand)       
 
-    def Plot(self, dsets, ol):
-        MetaData = pd.read_csv('%s_MetaData.txt'%(self.outDsets), header=None)
+    def Plot(self, dsets, ol, Process_):
+        nodes, nEvents = GetPlotVals(self.outDsets, dsets, Process_)
 
-        datasets = dsets[0].tolist()
-        nodes = [str(fullSt.split('/')[1].split('_')[2]) for fullSt in datasets]
-        nEvents = [int(nEvent_str.split(':')[-1]) for nEvent_str in MetaData[12]]
-
+        ##-- Set Label Font Size based on number of nodes 
         fontsize = 8 
-        if(len(nodes) > 15): fontsize = 6
+        if( (len(nodes) >= 6) and len(nodes) <= 13): fontsize = 9
+        elif(len(nodes) <= 5): fontsize = 12
 
         x_pos = [i for i, _ in enumerate(nodes)]
         y_pos = np.arange(len(nodes))
@@ -52,11 +64,48 @@ class CheckMC:
         plt.title('%s MiniAOD Nevents'%(self.outDsets))
         plt.hlines(400000,-0.5,len(nodes), linestyles='dashed', colors='black')
 
-        plt.savefig("%s/%s_nEvents.png"%(ol,self.outDsets))
+        plt.savefig("%s/%s-%s_nEvents.png"%(ol,Process_,self.outDsets))
         plt.close()
 
         # add time stamp to plot 
         # Could also be useful to have three bar plot. One for each year or final state. 
+
+def GetPlotVals(outDsets_, dsets_, Process):
+    nodes_, nEvents_ = [], [] 
+    MetaData = pd.read_csv('MetaData/%s_MetaData.txt'%(outDsets_), header=None)
+
+    datasets = dsets_[0].tolist()
+    nodes = [str(fullSt.split('/')[1].split('_')[2]) for fullSt in datasets]
+    nEvents = [int(nEvent_str.split(':')[-1]) for nEvent_str in MetaData[12]] ##-- 12: nEvents 
+    node_nEvt_pairs = zip(nodes,nEvents)
+
+    if(Process == "GF_LO"):
+        for pair in node_nEvt_pairs:
+            node, nEvent = pair
+            if(not "cHHH" in node):
+                nodes_.append(node)
+                nEvents_.append(nEvent)
+    elif(Process == "GF_NLO"):
+        for pair in node_nEvt_pairs:
+            node, nEvent = pair
+            if("cHHH" in node):
+                nodes_.append(node)
+                nEvents_.append(nEvent) 
+    elif(Process == "VBF_LO"):
+        for pair in node_nEvt_pairs:
+            node, nEvent = pair
+            if("C2V" in node):
+                nodes_.append(node)
+                nEvents_.append(nEvent)                   
+                             
+    return nodes_, nEvents_  
+
+def GetProcessLabel(Process):
+    outName_Process_Dict = {
+        "*_node_*" : "GF",
+        "*CV_*_C2V_*_C3_*_*" : "VBF"
+    }    
+    return outName_Process_Dict[Process]
 
 def GetFSLabel(fullFS):
     outName_FS_Dict = {
@@ -74,3 +123,18 @@ def GetYearLabel(fullYear):
     }   
     return outName_year_Dict[fullYear]    
       
+def CheckMakeOutLocation(outLoc_):
+    if(not os.path.exists(outLoc_)):
+        print"Creating output directory: %s"%(outLoc_)
+        beforeOutLoc = "%s/../"%(outLoc_)
+        os.system('mkdir %s'%(outLoc_))
+        os.system('cp %s/index.php %s'%(beforeOutLoc,outLoc_))  
+
+def GetProcesses(PhysicsType):
+    GF_dsetStr = "*_node_*"
+    VBF_dsetStr = "*CV_*_C2V_*_C3_*_*"
+
+    processDict = {
+        "NonResonant" : [GF_dsetStr, VBF_dsetStr]
+    }   
+    return processDict[PhysicsType]     
