b8a20951ef1f780aa59b6c6638f2a318158e3923
Abe Tishelman-Charny <abe2323@yahoo.com>
2021-11-09 11:08:58 +0100

    Adding Directory for HIG-21-014 post preapproval talk checks

---
+++ HIG-21-014/Post-PreAppTalk-Checks/Combine_NLO.py
@@ -0,0 +1,12 @@
+"""
+9 November 2021 
+Abraham Tishelman-Charny 
+
+The purpose of this module is to combine NLO samples while maintaining normalization, in order to use for reweighting to additional EFT benchmarks for HIG-21-014. 
+"""
+
+# Define file paths 
+d = "/eos/user/p/pmandrik/HHWWgg_central/January_2021_Production_v2/2017/Signal/SL_NLO_2017_hadded/"
+f1 = "%s/GluGluToHHTo2G2Qlnu_node_cHHH1_2017.root"
+f2 = "%s/GluGluToHHTo2G2Qlnu_node_cHHH2p45_2017.root"
+
